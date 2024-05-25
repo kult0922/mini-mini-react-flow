81e95eecdcc61c23707e6d7a14b33be9673b8eeb
kurisaki_kazuma <38486600+kult0922@users.noreply.github.com>
2024-05-25 16:09:56 +0900

    edge customize

--- src/edge/EdgeComponent.tsx
+++ src/edge/EdgeComponent.tsx
@@ -5,13 +5,22 @@ type Props = {
 };
 
 export const EdgeComponent = ({ from, to }: Props) => {
+  const c1 = {
+    x: from.x + Math.abs(to.x - from.x),
+    y: from.y,
+  };
+  const c2 = {
+    x: to.x - Math.abs(to.x - from.x),
+    y: to.y,
+  };
   return (
     <div className="edge-wrapper">
       <svg className="svg">
         <path
           stroke="black"
           strokeWidth="2"
-          d={`M ${from.x}, ${from.y} L ${to.x}, ${to.y}`}
+          fill="none"
+          d={`M ${from.x} ${from.y} C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y}`}
         />
       </svg>
     </div>
